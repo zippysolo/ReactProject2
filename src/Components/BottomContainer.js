41d5066248cdd89aa132a1103f17c917dc97232b
zippysolo <z811043@gmail.com>
2024-07-10 21:39:25 +0300

    testing

--- src/Components/BottomContainer.js
+++ src/Components/BottomContainer.js
@@ -3,27 +3,24 @@
 import TransBox from "./TransBox";
 import TotalsBox from "./TotalsBox"
 
-function BottomContainer()
-{
-
-
-    
-    return(
+function BottomContainer() {
+  //ddddddd
+  return (
     <div class="bottom-container">
-    <div class="bottom-container__left">
-      <div class="box total-box">
-        <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
+      <div class="bottom-container__left">
+        <div class="box total-box">
+          <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
             to last year">
-        </TotalsBox>
-        <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
+          </TotalsBox>
+          <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
             to last year">
-        </TotalsBox>
+          </TotalsBox>
+        </div>
       </div>
+      <TransBox headerText="Donations History"></TransBox>
+      <TransBox headerText="Income History"></TransBox>
     </div>
-    <TransBox headerText="Donations History"></TransBox>
-    <TransBox headerText="Income History"></TransBox>
-    </div>
-    );
+  );
 }
 
 export default BottomContainer;
