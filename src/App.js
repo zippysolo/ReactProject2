239c813899e9a4b7fd3abc714d08e44686f2bc79
Zippy S <z811043@gmail.com>
2024-07-10 12:10:57 +0300

    more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './Components/Card';
 import Sidebar from './Components/Sidebar';
 import Topbar from './Components/Topbar';
-import TotalsBox from './Components/TotalsBox';
-import TransBox from './Components/TransBox';
+
+import BottomContainer from './Components/BottomContainer';
 
 function App() {
   return (
@@ -12,25 +11,8 @@ function App() {
       <Sidebar></Sidebar>
       <main className="main-content">
         <Topbar></Topbar>
-        <div class="bottom-container">
-
-          <div class="bottom-container__left">
-            <div class="box total-box">
-              <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
-                  to last year">
-              </TotalsBox>
-              <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
-                  to last year">
-              </TotalsBox>
-
-            </div>
-
-          </div>
-          <TransBox headerText="Donations History"></TransBox>
-          <TransBox headerText="Income History"></TransBox>
-        </div>
+        <BottomContainer></BottomContainer>
       </main>
-
     </div>
   );
 }
---
+++ src/Components/BottomContainer.js
@@ -0,0 +1,27 @@
+
+
+import TransBox from "./TransBox";
+import TotalsBox from "./TotalsBox"
+
+
+function BottomContainer()
+{
+    return(
+    <div class="bottom-container">
+    <div class="bottom-container__left">
+      <div class="box total-box">
+        <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
+            to last year">
+        </TotalsBox>
+        <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
+            to last year">
+        </TotalsBox>
+      </div>
+    </div>
+    <TransBox headerText="Donations History"></TransBox>
+    <TransBox headerText="Income History"></TransBox>
+    </div>
+    );
+}
+
+export default BottomContainer;
